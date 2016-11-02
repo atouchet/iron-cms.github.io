faa5abb6d75c9a831d1f938676d1c7572e66e853
Evgeny Ukhanov <mrlsd@ya.ru>
2016-11-02 22:51:44 +0200

    Docs for v0.2.10 - fix traits

---
+++ doc/implementors/iron_cms/middleware/trait.Decodable.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["iron_cms"] = ["impl&lt;T:&nbsp;<a class='trait' href='iron_cms/middleware/trait.Decodable.html' title='iron_cms::middleware::Decodable'>Decodable</a>&gt; <a class='trait' href='iron_cms/middleware/trait.Decodable.html' title='iron_cms::middleware::Decodable'>Decodable</a> for <a class='struct' href='iron_cms/middleware/struct.Validator.html' title='iron_cms::middleware::Validator'>Validator</a>&lt;T&gt;","impl <a class='trait' href='iron_cms/middleware/trait.Decodable.html' title='iron_cms::middleware::Decodable'>Decodable</a> for <a class='struct' href='iron_cms/middleware/struct.ErrorValidator.html' title='iron_cms::middleware::ErrorValidator'>ErrorValidator</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
